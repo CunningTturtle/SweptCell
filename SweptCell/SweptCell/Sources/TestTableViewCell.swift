db71530256ea4173fabd1145d47e6d8b66c6be64
phone001 <2008@qq.com>
2021-04-26 16:10:33 +0800

    bug修复

---
+++ SweptCell/SweptCell/Sources/TestTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TestTableViewCell.swift
+//  SweptCell
+//
+//  Created by admin on 2021/4/26.
+//
+
+import UIKit
+
+public class TestTableViewCell: UITableViewCell {
+
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    public override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
