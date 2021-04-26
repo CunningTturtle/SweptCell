e825aedd37526d679f0190f2619f8050157d3990
phone001 <2008@qq.com>
2021-04-26 14:29:55 +0800

    bug 修复

--- Sources/WLTableViewCell.swift
+++ Sources/WLTableViewCell.swift
@@ -29,7 +29,7 @@ open class WLTableViewCell: UITableViewCell {
         }
     }
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         addSubview(scroll)
